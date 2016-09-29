03960bf36cabc5ee2afb11a717e406dcc214edd8
IanBadcoe <ian_badcoe@yahoo.co.uk>
2016-09-29 11:57:15 +0100

    Navigation working

--- js/app.js
+++ js/app.js
@@ -2,6 +2,7 @@
     angular.module('app', ["page1", "page2", "page3"])
         .controller('pageController', ['$scope', '$location', '$window', function ($scope, $location, $window) {
             $scope.myPage = 1;
+            $scope.pageNavigate = function(i) { $scope.myPage = i; };
         }
         ])
 })();
