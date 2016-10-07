3a9e6be97520cf8bfb10ffe2a594a5b74fb1acdb
Ian Badcoe <IBadcoe@Sumo-Digital.com>
2016-10-07 15:52:09 +0100

    got animation div sized correctly more improved fog

--- js/page1.js
+++ js/page1.js
@@ -5,7 +5,32 @@
         templateUrl: "templates/page1template.html",
         restrict: "E",
         controller: ['$scope', '$element', function ($scope, $element) {
-        }]
+        }],
+        link: function(scope, elem, attrs) {
+          background_ng_elem = elem.find(".fog-background");
+          anim_ng_elem = angular.element(elem).find(".anim-div");
+          scope.$watch(function() {
+            var f_dom = background_ng_elem.get(0);
+
+            var l = f_dom.offsetLeft;
+            var t = f_dom.offsetTop;
+            var w = f_dom.offsetWidth;
+            var h = f_dom.offsetHeight;
+
+            return { l: l, t: t, w: w, h: h }
+          },
+          function(newValue)
+          {
+            anim_ng_elem.css("left", newValue.l + "px");
+            anim_ng_elem.css("top", newValue.t + "px");
+            anim_ng_elem.css("width", newValue.w + "px");
+            anim_ng_elem.css("height", newValue.h + "px");
+          }, true);
+
+          background_ng_elem.on("load", function() {
+            scope.$apply();
+          });
+        }
       };
     });
 })();
