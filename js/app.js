6de7fc7103337a78692dae19e68c23eb592b39fe
Ian Badcoe <IBadcoe@Sumo-Digital.com>
2016-10-13 16:25:16 +0100

    removed commented code

--- js/app.js
+++ js/app.js
@@ -1,25 +1,6 @@
 'use strict';
 
 (function () {
-/*    var sound = new Howl({
-        src: ['sound/pin.mp3'],
-        panningModel: 'HRTF',
-        refDistance: 200,
-        rolloffFactor: 1,
-        distanceModel: 'linear'
-    });
-
-    function pin() {
-        var x = Math.random() * 1000 - 500;
-        var y = Math.random() * 1000 - 500;
-        sound.pos(x, y, 0);
-        sound.play();
-    };
-
-    sound.on('end', function(){
-        pin();
-    }); */
-
     angular.module('app', ["page1", "page2", "page3"])
         .controller('pageController', ['$scope', '$location', '$window', function ($scope, $location, $window) {
             $scope.myPage = 1;
