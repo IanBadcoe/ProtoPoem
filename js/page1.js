024d69d8f39a581558d7d3fd6822215cb2566978
Ian Badcoe <IBadcoe@Sumo-Digital.com>
2016-10-14 10:05:58 +0100

    reworked sizing to calculate the size/position of a single element - based off the window size - allowing for the nav-bar height - maintaining a 16/9 ratio

--- js/page1.js
+++ js/page1.js
@@ -8,32 +8,7 @@
           page1soundscape.start();
 
           $element.on("$destroy", function() { page1soundscape.end(); });
-        }],
-        link: function(scope, elem, attrs) {
-          background_ng_elem = elem.find(".fog-background-dummy");
-          anim_ng_elem = angular.element(elem).find(".anim-div");
-          scope.$watch(function() {
-            var f_dom = background_ng_elem.get(0);
-
-            var l = f_dom.offsetLeft;
-            var t = f_dom.offsetTop;
-            var w = f_dom.offsetWidth;
-            var h = f_dom.offsetHeight;
-
-            return { l: l, t: t, w: w, h: h }
-          },
-          function(newValue)
-          {
-            anim_ng_elem.css("left", newValue.l + "px");
-            anim_ng_elem.css("top", newValue.t + "px");
-            anim_ng_elem.css("width", newValue.w + "px");
-            anim_ng_elem.css("height", newValue.h + "px");
-          }, true);
-
-          background_ng_elem.on("load", function() {
-            scope.$apply();
-          });
-        }
+        }]
       };
     });
 })();
