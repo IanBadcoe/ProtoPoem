943a138294c272131ac5abdab27c3aa1ea906e6b
Ian Badcoe <IBadcoe@Sumo-Digital.com>
2016-11-03 14:04:44 +0000

    not quite working sparkle init

--- js/page1.js
+++ js/page1.js
@@ -18,110 +18,147 @@
 
     function randomElement(ary) { return ary[Math.floor(Math.random() * ary.length)]; }
 
-    function setSparkleLoopRunning($scope, $timeout, $rootScope) {
-        function sparkleStep(phrase)
+    function sparkleStep(phrase)
+    {
+        if (reverse_step > 0 && phrase.x < reverse_step)
         {
-            if (reverse_step > 0 && phrase.x < reverse_step)
-            {
-                phrase.x -= phrase.v * v_scale * step_scale;
-            }
-            else if (phrase.loopAng > 0)
-            {
-                // everything about 2PI cancels and if we want to go around the circle
-                // at the same speed we were moving, only the radius and speed matter to the step_size
-                phrase.loopAng -= v_scale / phrase.loopXSize * step_scale;
-                var sa = Math.sin(-phrase.loopAng);
-                var ca = Math.cos(-phrase.loopAng);
-
-                phrase.x += ca * phrase.v * phrase.loopXSize;
-                phrase.y += sa * phrase.v * phrase.loopYSize;
-            }
-            else
-            {
-                if (Math.random() < time_step * 0.0006)
-                {
-                    phrase.loopAng = 2 * Math.PI;
-                    phrase.loopXSize = (Math.random() + 1) / 2;
-                    phrase.loopYSize = phrase.loopXSize * (Math.random() > 0.5 ? -1 : 1) * $rootScope.aspect_ratio;
-                }
-
-                phrase.x += phrase.v * v_scale * step_scale;
-            }
-
-            phrase.y += Math.sin(phrase.x * phrase.wobble_freq + phrase.wobble_phase) * step_scale * phrase.wobble_scale * $rootScope.aspect_ratio;
-            phrase.angle += phrase.rotation * step_scale;
+            phrase.x -= phrase.v * v_scale * step_scale;
         }
-        function sparkleLoop() {
-            sparkle_loop_cycle = (sparkle_loop_cycle + 1) % 10;
+        else if (phrase.loopAng > 0)
+        {
+            // everything about 2PI cancels and if we want to go around the circle
+            // at the same speed we were moving, only the radius and speed matter to the step_size
+            phrase.loopAng -= v_scale / phrase.loopXSize * step_scale;
+            var sa = Math.sin(-phrase.loopAng);
+            var ca = Math.cos(-phrase.loopAng);
 
-            if (reverse_step > -100)
+            phrase.x += ca * phrase.v * phrase.loopXSize;
+            phrase.y += sa * phrase.v * phrase.loopYSize;
+        }
+        else
+        {
+            if (Math.random() < time_step * 0.0006)
             {
-                reverse_step -= 3;
+                phrase.loopAng = 2 * Math.PI;
+                phrase.loopXSize = (Math.random() + 1) / 2;
+                phrase.loopYSize = phrase.loopXSize * (Math.random() > 0.5 ? -1 : 1) * $rootScope.aspect_ratio;
+            }
+
+            phrase.x += phrase.v * v_scale * step_scale;
+        }
+
+        phrase.y += Math.sin(phrase.x * phrase.wobble_freq + phrase.wobble_phase) * step_scale * phrase.wobble_scale * $rootScope.aspect_ratio;
+        phrase.angle += phrase.rotation * step_scale;
+    }
+
+    function sparkleLoopAddOne(phrases, atStart)
+    {
+        var hp = phrases.filter(function (x) { return !(x.found || x.in_use) });
+
+        if (hp.length != 0 && num_sparkles < 10) {
+            var phrase = randomElement(hp);
+
+            phrase.in_use = true;
+            phrase.loopAng = -1;
+            phrase.wobble_scale = (Math.random() + 1) / 5;
+            phrase.wobble_phase = Math.random() * 2 * Math.PI;
+            phrase.wobble_freq = (Math.random() + 0.5) / 4;
+            phrase.angle = Math.random() * Math.PI * 2;
+            phrase.rotation = (Math.random() - 0.3) * 20;
+
+            if (atStart)
+            {
+                phrase.x = 0;
             }
             else
             {
-                if (Math.random() < time_step * 0.0001) reverse_step = 100;
+                phrase.x = Math.random() * 100;;
             }
+            phrase.y = Math.random() * 100;
 
-            if (!sparkle_loop_cycle) {
-                var hp = phrases.filter(function (x) { return !(x.found || x.in_use) });
+            var plane_idx = Math.floor(Math.random() * 4);
 
-                if (hp.length != 0 && num_sparkles < 10) {
-                    var phrase = randomElement(hp);
+            phrase.v = sparkle_plane_scales[plane_idx];
+            phrase.w = phrase.v * 10;
 
-                    phrase.in_use = true;
-                    phrase.loopAng = -1;
-                    phrase.wobble_scale = (Math.random() + 1) / 5;
-                    phrase.wobble_phase = Math.random() * 2 * Math.PI;
-                    phrase.wobble_freq = (Math.random() + 0.5) / 4;
-                    phrase.angle = Math.random() * Math.PI * 2;
-                    phrase.rotation = (Math.random() - 0.3) * 20;
+            $scope.phrases[plane_idx].push(phrase);
+            num_sparkles++;
+        }
 
-                    phrase.x = 0;
-                    phrase.y = Math.random() * 100;
+    }
 
-                    var plane_idx = Math.floor(Math.random() * 4);
+    function sparkleLoop($scope) {
+        sparkle_loop_cycle = (sparkle_loop_cycle + 1) % 10;
 
-                    phrase.v = sparkle_plane_scales[plane_idx];
-                    phrase.w = phrase.v * 10;
+        if (reverse_step > -100)
+        {
+            reverse_step -= 3;
+        }
+        else
+        {
+            if (Math.random() < time_step * 0.0001) reverse_step = 100;
+        }
 
-                    $scope.phrases[plane_idx].push(phrase);
-                    num_sparkles++;
-                }
-            }
+        if (!sparkle_loop_cycle) {
+            sparkleLoopAddOne($scope.phrases, true);
+        }
 
-            for(var i = 0; i < 4; i++)
+        for(var i = 0; i < 4; i++)
+        {
+            for(var j = 0; j < $scope.phrases[i].length; )
             {
-                for(var j = 0; j < $scope.phrases[i].length; )
+                var phrase = $scope.phrases[i][j];
+
+                sparkleStep(phrase);
+
+                phrase.style = {
+                    left: "{x}%".format(phrase),
+                    top: "{y}%".format(phrase),
+                    width: "{w}%".format(phrase),
+                    transform: "rotate({angle}deg)".format(phrase)
+                };
+
+                if (phrase.x > 100 || phrase.x < -20 || phrase.y > 110 || phrase.y < -10 )
                 {
-                    var phrase = $scope.phrases[i][j];
-
-                    sparkleStep(phrase);
-
-                    phrase.style = {
-                        left: "{x}%".format(phrase),
-                        top: "{y}%".format(phrase),
-                        width: "{w}%".format(phrase),
-                        transform: "rotate({angle}deg)".format(phrase)
-                    };
-
-                    if (phrase.x > 100 || phrase.x < -20 || phrase.y > 110 || phrase.y < -10 )
-                    {
-                        $scope.phrases[i].splice(j, 1);
-                        phrase.in_use = false;
-                        num_sparkles--;
-                    }
-                    else
-                    {
-                        j++;
-                    }
+                    $scope.phrases[i].splice(j, 1);
+                    phrase.in_use = false;
+                    num_sparkles--;
+                }
+                else
+                {
+                    j++;
                 }
             }
+        }
 
-            if (!$scope.terminate) {
-                $timeout(sparkleLoop, time_step);
-            }
-        };
+        if (!$scope.terminate) {
+            $timeout(sparkleLoop, time_step);
+        }
+    };
+
+    function resetSparkles($scope)
+    {
+        phrases.forEach(function(phrase) {
+            phrase.in_use = false;
+        });
+
+        $scope.phrases = [[], [], [], []];
+
+        num_sparkles = 0;
+    }
+
+    function initSubPageSparkles($scope, subPage)
+    {
+        resetSparkles($scope);
+
+        for(var i = 0; i < 10; i++)
+        {
+            sparkleLoopAddOne($scope.phrases, false);
+        }
+    }
+
+    function setSparkleLoopRunning($scope, $timeout, $rootScope) {
+        initSubPageSparkles($scope.mySubPage);
 
         $timeout(sparkleLoop, time_step);
     };
@@ -149,6 +186,7 @@
 
                         $scope.nextSubPage = function() {
                             $scope.mySubPage = $scope.mySubPage % 3 + 1;
+                            initSubPageSparkles($scope.mySubPage);
                         };
 
                         $scope.mySubPage = 1;
