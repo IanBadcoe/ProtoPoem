e602754cba5df7f928422be479f007f1cda7a07c
Ian Badcoe <IBadcoe@Sumo-Digital.com>
2017-02-16 14:03:27 +0000

    working on page-2 parallax

--- js/page2.js
+++ js/page2.js
@@ -2,17 +2,29 @@
 
 (function () {
    angular.module("page2", [])
-	.directive("page2", function () {
+	.directive("page2", [ "$rootScope", function ($rootScope) {
 	   return {
 	      templateUrl: "/templates/page2template.html",
 	      restrict: "E",
 	      controller: ['$scope', '$element', function ($scope, $element) {
               $scope.mySubPage = 1;
 
-              $scope.onMouseMove = function() {
-                  alert("mouse moved");
+              $scope.far_parallax_x = "7.5%";
+              $scope.near_parallax_x = "7.5%";
+
+              $scope.onMouseMove = function(e) {
+                  // image is visible between 15% and 100% of the parent div
+                  var fx = (e.clientX - $rootScope.live_left) / $rootScope.live_width;
+                  fx = (fx - 0.15) / 0.85;
+
+                  var parallax = (fx - 0.5) * 15;
+
+                  var vals = { near: -parallax, far: parallax}
+
+                  $scope.far_parallax_x = "{far}%";
+                  $scope.near_parallax_x = "{near}%";
               };
 	      }]
 	   };
-	});
+	}]);
 })();
