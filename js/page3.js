c95fb13dac09c0a647ebdfc90d70b4e18ef2f888
Ian Badcoe <IBadcoe@Sumo-Digital.com>
2017-02-17 13:48:07 +0000

    and page-3 template, while I'm at it...

--- js/page3.js
+++ js/page3.js
@@ -4,7 +4,7 @@
    angular.module("page3", [])
 	.directive("page3", function () {
 	   return {
-	      templateUrl: "/templates/page3template.html",
+	      templateUrl: "templates/page3template.html",
 	      restrict: "E",
 	      controller: ['$scope', '$element', function ($scope, $element) {
 	      }]
