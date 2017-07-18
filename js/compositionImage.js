c0150e34be66be8b7ea97c5a470e1e52292fe249
Ian Badcoe <IBadcoe@Sumo-Digital.com>
2017-07-18 14:04:52 +0100

    nearly got compositionImage scroll working

--- js/compositionImage.js
+++ js/compositionImage.js
@@ -1,23 +1,38 @@
 (function () {
-  angular.module("compositionImageModule", [])
-    .directive("compositionImage", function () {
-      return {
-        templateUrl: "templates/compositionImageTemplate.html",
-        restrict: "E",
-        scope: {
-//            left: "=",
-//            bottom: "=",
-//            height: "=",
-            image: "@",
-//            highlit_image: "@",
-//            scroll: "="
-        },
-        controller: ['$scope', '$element', function ($scope, $element) {
-            if ($scope.highlit_image === null)
-            {
-                $scope.highlit_image = $scope.image;
-            }
-        }]
-      };
-    });
+    function recalcStyle(scope, rootScope) {
+        var aspect = scope.config.scroll_width / 900;
+
+        var inner_aspect_ratio = rootScope.aspect_ratio * 0.85;
+
+        // each image has aspect_ratio - screen_aspect_ratio sticking out beyond the screen edge
+        // only 15% larger since I put the phrase plane on the left
+        var extra = aspect - inner_aspect_ratio;
+
+        // so the max movement of each image as a % is 100 * that, and the current movement is -scroll * that
+        // HOWEVER divide by the aspect ratio as we've calculated all this as a fraction of the height
+        // not the width
+        var vals = {
+            left: -scope.config.scroll * extra / inner_aspect_ratio
+        };
+
+        scope.left = "{left}%".format(vals);
+    }
+
+    angular.module("compositionImageModule", [])
+        .directive("compositionImage", [ "$rootScope", function ($rootScope) {
+            return {
+                templateUrl: "templates/compositionImageTemplate.html",
+                restrict: "E",
+                scope: {
+                    config: "="
+                },
+                controller: ['$scope', '$element', function ($scope, $element) {
+                    $scope.left = "0%";
+
+                    $scope.$watch("config.scroll", function () {
+                        recalcStyle($scope, $rootScope);
+                    });
+                }]
+            };
+        }]);
 })();
--- js/page2.js
+++ js/page2.js
@@ -15,9 +15,11 @@
 
                     $scope.captions = ["Engine compartment"];
 
-                    $scope.far_parallax_x = "0%";
-                    $scope.mid_parallax_x = "0%";
-                    $scope.near_parallax_x = "0%";
+                    $scope.components = [
+                        { image: "img/page2-far.png", scroll_width: 2400 },
+                        { image: "img/page2-mid.png", scroll_width: 2640 },
+                        { image: "img/page2-near.png", scroll_width: 2880 },
+                    ];
 
                     var interval_promise = null;
                     var scroll = 0;
@@ -35,39 +37,9 @@
                         interval_promise = $interval(function () {
                             scroll = Math.min(Math.max(0, scroll + vel), 100);
 
-                            // scroll runs from 0 -> 100
-                            // images need to move their left by width - screen_width
-                            var far_aspect = 2400 / 900;
-                            var mid_aspect = 2640 / 900;
-                            var near_aspect = 2880 / 900;
-
-                            var inner_aspect_ratio = $rootScope.aspect_ratio * 0.85;
-
-                            // each image has aspect_ratio - screen_aspect_ratio sticking out beyond the screen edge
-                            // only 15% larger since I put the phrase plane on the left
-                            var far_extra = far_aspect - inner_aspect_ratio;
-                            var mid_extra = mid_aspect - inner_aspect_ratio;
-                            var near_extra = near_aspect - inner_aspect_ratio;
-
-                            // so the max movement of each image as a % is 100 * that, and the current movement is -scroll * that
-                            // HOWEVER divide by the aspect ratio as we've calculated all this as a fraction of the height
-                            // not the width
-                            var vals = {
-                                near: -scroll * near_extra / inner_aspect_ratio,
-                                mid: -scroll * mid_extra / inner_aspect_ratio,
-                                far: -scroll * far_extra / inner_aspect_ratio,
-                                cl: (-scroll * near_extra / inner_aspect_ratio) + 78.06,
-                                cl2: (-scroll * near_extra / inner_aspect_ratio) + 83.81
-                            }
-
-                            $scope.far_parallax_x = "{far}%".format(vals);
-                            $scope.mid_parallax_x = "{mid}%".format(vals);
-                            $scope.near_parallax_x = "{near}%".format(vals);
-                            $scope.cl_parallax_x = "{cl}%".format(vals);
-                            $scope.cl_parallax_x2 = "{cl2}%".format(vals);
-                            $scope.cl_top = "51.5%";
-                            $scope.cl_bottom = "63.1%";
-
+                            $scope.components.forEach(function(component) {
+                                component.scroll = scroll;
+                            }, this);
                         }, 10);
                     }
 
@@ -83,7 +55,7 @@
                         cancelMove();
                     };
 
-                    startMove(0);
+//                    startMove(0);
                 }]
             };
         }]);
