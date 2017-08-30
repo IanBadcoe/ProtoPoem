ecbd6a9425fb12e4a98048dd663039f5467647f2
Ian Badcoe <IBadcoe@Sumo-Digital.com>
2017-08-30 16:45:17 +0100

    images

--- js/page2.js
+++ js/page2.js
@@ -20,10 +20,11 @@
                     $scope.floor = { image: "img/floor.png", front_scroll_width: 2880, back_scroll_width: 2400 };
 
                     $scope.components = [
+                        { image: "img/panel.png", image_hl: "img/panel-hl.png", scroll_width: 2400, left: 1400, bottom: 450 },
 //                        { image: "img/page2-mid.png", scroll_width: 2640 },
                         { image: "img/instruments.png", image_hl: "img/instruments-hl.png", scroll_width: 2650, left: -50, bottom: 15 },
-                        { image: "img/jengine.png", image_hl: "img/jengine-hl.png", scroll_width: 2760, left: 900, bottom: 5 },
-                        { image: "img/cabinet.png", image_hl: "img/cabinet-hl.png", scroll_width: 2880, left: 2203, bottom: 0 },
+                        { image: "img/cabinet.png", image_hl: "img/cabinet-hl.png", scroll_width: 2650, left: 2203, bottom: 80 },
+                        { image: "img/jengine.png", image_hl: "img/jengine-hl.png", scroll_width: 2760, left: 700, bottom: 5 },
                         { image: "img/page2-near.png", scroll_width: 2880 },
                         { image: "img/check-list.png", scroll_width: 2880, left: 1225, bottom: 350 },
                         { image: "img/pillar.png", image_hl: "img/pillar-hl.png", scroll_width: 2950, left: -70, bottom: -100 },
